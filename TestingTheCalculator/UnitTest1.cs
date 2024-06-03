73ff264bc324bc97bd9f96f941dce2164326fc03
kdulevich <kdulevich@BPLPW-KDULEVICH>
2024-06-04 00:36:24 +0300

    Add project files.

---
+++ TestingTheCalculator/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using PageObjLib.Factories;
+using PageObjLib.Page;
+using SeleniumExtras.WaitHelpers;
+using Xunit;
+
+namespace TestingTheCalculator
+{
+    public class Tests
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+           Page.GoUrl("https://www.desmos.com/scientific?lang=ru");
+        }
+
+
+
+        [Fact(DisplayName = "ClearAllButtonPositive")]
+        [Test]
+        public void ClearButtonTest1()
+        {
+            WebElement element = new();
+
+            element.dicOfButtons["1"].Click();
+            element.dicOfButtons["+"].Click();
+            element.dicOfButtons["1"].Click();
+
+            var result = element.ClearAllButton().Displayed;
+
+            Assert.IsTrue(result);
+        }
+
+        [Fact(DisplayName = "OutputContainer")]
+        [Test]
+        public void OutputTest1()
+        {
+            WebElement element = new();
+
+            element.dicOfButtons["8"].Click();
+            element.dicOfButtons["×"].Click();
+            element.dicOfButtons["9"].Click();
+
+            var result = element.Output().Displayed;
+
+            Assert.IsTrue(result);
+        }
+
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.QuitDriver();
+            Driver.DisposeDriver();
+        }
+
+    }
+}
---
+++ TestingTheCalculator/WebElement.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using PageObjLib.Factories;
+using PageObjLib.Page;
+using SeleniumExtras.WaitHelpers;
+
+namespace TestingTheCalculator
+{
+    public class WebElement
+    {
+        public Dictionary<string, IWebElement> dicOfButtons {get; set;}
+
+        public WebElement()
+        {
+            dicOfButtons ??= GetButtons();
+        }
+        public Dictionary<string, IWebElement> GetButtons()
+        {
+            var allButtons = Driver.GetWait().Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//span[@dcg-command]"))).DistinctBy(a => a.Text).ToList();
+            return allButtons.ToDictionary(p => p.Text);
+        }
+
+        public IWebElement ClearAllButton() => Page.GetElement(By.XPath("//div[text()='стереть все']"));
+
+        public IWebElement Output() => Page.GetElement(By.ClassName("dcg-exp-output-container"));
+    }
+}
