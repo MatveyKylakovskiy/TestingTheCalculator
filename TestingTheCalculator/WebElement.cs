7ab52e11d37614245de9c50df327d28d7215bd21
kdulevich <kdulevich@BPLPW-KDULEVICH>
2024-06-04 23:06:31 +0300

    Final

--- TestingTheCalculator/WebElement.cs
+++ TestingTheCalculator/WebElement.cs
@@ -20,8 +20,14 @@ namespace TestingTheCalculator
         }
 
         public IWebElement ClearAllButton() => Page.GetElement(By.XPath("//div[text()='стереть все']"));
+        public IWebElement InputBar() => Page.GetElement(By.XPath("//span[@class='dcg-mq-root-block dcg-mq-empty']"));
+        public IWebElement OutputBar() => Page.GetElement(By.ClassName("dcg-exp-output-container"));
+        public IWebElement ErrorIcon() => Page.GetElement(By.ClassName("dcg-icon-error"));
+        public IWebElement AbcButton() => Page.GetElement(By.XPath("//*[text()='abc']"));
+        public IWebElement A_Button() => Page.GetElement(By.XPath("//span[@dcg-command='a']"));
 
-        public IWebElement Output() => Page.GetElement(By.ClassName("dcg-exp-output-container"));
-        //Поиск результата
+        public IWebElement SettingsButton() => Page.GetElement(By.XPath("//div[@dcg-command='settings']"));
+        public IWebElement ContrastButton() => Page.GetElement(By.ClassName("dcg-checkbox"));
+        public IWebElement CheckBoxContrast() => Page.GetElement(By.XPath("//div[@class='dcg-component-checkbox dcg-checked dcg-settings-menu-option dcg-do-not-blur dcg-reverse-contrast']"));
     }
 }
